fc180fbb99510c7038dab0b347b4bc22e3ed53f5
code4human <tena.kim10@gmail.com>
2021-04-25 15:57:14 +0900

    Fix the name error and parameter of cast_single_ray

--- 3.height/cast.c
+++ 3.height/cast.c
@@ -1,6 +1,6 @@
 #include "height_header.h"
 
-double	cast_single_ray(int x, double px, double py, double theta)
+double	cast_single_ray(int x, double px, double py, double theta, dir_t* pdir)
 {
 	double ray = (theta + FOV_H_DIV2) - (x * ANGLE_PER_PIXEL);
 
@@ -10,7 +10,7 @@ double	cast_single_ray(int x, double px, double py, double theta)
 	if (get_wall_intersection(ray, px, py, &wdir, &wx, &wy) == false)
 		return INFINITY;
 	double wdist = l2dist(px, py, wx, wy);
-	wdist *= cos(th - ray);   // 벽과의 거리를 평면으로 보정
+	wdist *= cos(theta - ray);   // 벽과의 거리를 평면으로 보정
 
 	return wdist;
 }
